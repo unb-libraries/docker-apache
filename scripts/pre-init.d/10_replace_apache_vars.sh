0a3725787a2838f2f3ae61e4eb288f5e1147e717
Jacob Sanford <jsanford@unb.ca>
2016-02-22 15:28:21 -0400

    Shebang updates

--- scripts/pre-init.d/10_replace_apache_vars.sh
+++ scripts/pre-init.d/10_replace_apache_vars.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 sed -i "s|APP_HOSTNAME|$APP_HOSTNAME|g" /etc/apache2/conf.d/vhost.conf
 sed -i "s|APP_WEBROOT|$APP_WEBROOT|g" /etc/apache2/conf.d/vhost.conf
 sed -i "s|APACHE_ERROR_LOG_FILE|$APACHE_ERROR_LOG_FILE|g" /etc/apache2/conf.d/vhost.conf
