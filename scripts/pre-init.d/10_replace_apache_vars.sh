7742705c22dac93309fec0c348dd6ae426c00746
Jacob Sanford <jsanford@unb.ca>
2016-02-23 08:50:26 -0400

    Find/replace in proper conf file to LOG vars

--- scripts/pre-init.d/10_replace_apache_vars.sh
+++ scripts/pre-init.d/10_replace_apache_vars.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 if [ "$DEPLOY_ENV" = "dev" ]; then
-  sed -i "s|APACHE_LOG_FILE|/proc/self/fd/2|g" /etc/nginx/conf.d/app.conf
-  sed -i "s|APACHE_ERROR_LOG_FILE|/proc/self/fd/2|g" /etc/nginx/conf.d/app.conf
+  sed -i "s|APACHE_LOG_FILE|/proc/self/fd/2|g" /etc/apache2/conf.d/app.conf
+  sed -i "s|APACHE_ERROR_LOG_FILE|/proc/self/fd/2|g" /etc/apache2/conf.d/app.conf
 else
   sed -i "s|APACHE_ERROR_LOG_FILE|$APACHE_ERROR_LOG_FILE|g" /etc/apache2/conf.d/app.conf
   sed -i "s|APACHE_LOG_FILE|$APACHE_LOG_FILE|g" /etc/apache2/conf.d/app.conf
