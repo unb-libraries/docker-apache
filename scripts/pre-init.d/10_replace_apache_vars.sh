17d1e35f1bfd683455a70d4a7727a8d4c90f78d7
Jacob Sanford <jsanford@unb.ca>
2016-06-04 22:09:13 -0300

    Standardize on app logging to docker log.

--- scripts/pre-init.d/10_replace_apache_vars.sh
+++ scripts/pre-init.d/10_replace_apache_vars.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env sh
-if [ "$DEPLOY_ENV" = "dev" ]; then
-  sed -i "s|APACHE_LOG_FILE|/proc/self/fd/2|g" /etc/apache2/conf.d/app.conf
-  sed -i "s|APACHE_ERROR_LOG_FILE|/proc/self/fd/2|g" /etc/apache2/conf.d/app.conf
-else
-  sed -i "s|APACHE_ERROR_LOG_FILE|$APACHE_ERROR_LOG_FILE|g" /etc/apache2/conf.d/app.conf
-  sed -i "s|APACHE_LOG_FILE|$APACHE_LOG_FILE|g" /etc/apache2/conf.d/app.conf
-fi
+sed -i "s|APACHE_ERROR_LOG_FILE|$APACHE_ERROR_LOG_FILE|g" /etc/apache2/conf.d/app.conf
+sed -i "s|APACHE_LOG_FILE|$APACHE_LOG_FILE|g" /etc/apache2/conf.d/app.conf
 
 sed -i "s|APP_HOSTNAME|$APP_HOSTNAME|g" /etc/apache2/conf.d/app.conf
 sed -i "s|APP_WEBROOT|$APP_WEBROOT|g" /etc/apache2/conf.d/app.conf
