d077bdbaf89705bc8d31a251bb68eeffcbed28d1
Jacob Sanford <jsanford@unb.ca>
2016-06-04 21:22:56 -0300

    Reference run and group user more sanely

--- scripts/pre-init.d/10_replace_apache_vars.sh
+++ scripts/pre-init.d/10_replace_apache_vars.sh
@@ -10,5 +10,7 @@ fi
 sed -i "s|APP_HOSTNAME|$APP_HOSTNAME|g" /etc/apache2/conf.d/app.conf
 sed -i "s|APP_WEBROOT|$APP_WEBROOT|g" /etc/apache2/conf.d/app.conf
 
-sed -i "s|User apache|User $APACHE_RUN_USER|g" /etc/apache2/httpd.conf
-sed -i "s|Group apache|Group $APACHE_RUN_GROUP|g" /etc/apache2/httpd.conf
+sed -i '/^User / d' /etc/apache2/httpd.conf
+sed -i '/^Group / d' /etc/apache2/httpd.conf
+sed -i "s|APACHE_RUN_USER|$APACHE_RUN_USER|g" /etc/apache2/conf.d/app.conf
+sed -i "s|APACHE_RUN_GROUP|$APACHE_RUN_GROUP|g" /etc/apache2/conf.d/app.conf
